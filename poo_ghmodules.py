9e71c41312ac4e6c02954552409a8dadd758c973
Poonacha <km.poonacha@gmail.com>
2019-04-25 15:12:16 +0200

    Too many changes to remember

---
+++ poo_ghmodules.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 22 10:37:38 2019
+
+@author: kmpoo
+"""
+import csv
+import requests
+from time import sleep
+
+
+
+TRIP = 0
+
+def getGitHubapi(url,PW_CSV,LOG_CSV):
+    """This function uses the requests.get function to make a GET request to the GitHub api
+    TRIP flag is used to toggle GitHub accounts. The max rate for searches is 30 per hr per account"""
+    global TRIP
+    """ Get PW info """
+    PW_list = []
+    
+    with open(PW_CSV, 'rt', encoding = 'utf-8') as PWlist:
+        PW_handle = csv.reader(PWlist)
+        del PW_list[:]
+        for pw in PW_handle:
+            PW_list.append(pw)
+    if TRIP == 0:
+        repo_req = requests.get(url, auth=(PW_list[0][0], PW_list[0][1]))
+        TRIP = 1
+    elif TRIP == 1:
+        repo_req = requests.get(url, auth=(PW_list[1][0], PW_list[1][1]))
+        TRIP = 2
+    else:
+        repo_req = requests.get(url, auth=(PW_list[2][0], PW_list[2][1]))        
+        TRIP = 0
+        
+    if repo_req.status_code == 200: 
+        print(repo_req.headers['X-RateLimit-Remaining'])
+        if int(repo_req.headers['X-RateLimit-Remaining']) <= 3:
+            """  Re-try if Github limit is reached """
+            print("************************************************** GitHub limit close t obeing reached.. Waiting for 10 mins" )
+            """ Provide a 10 mins delay if the limit is close to being reached  """
+            sleep(600)
+            
+        """ Return the requested data  """
+        return repo_req
+    else:
+        print("Error accessing url = ",url)
+        repo_json = repo_req.json()
+        print("Error code = ",repo_req.status_code,". Error message = ",repo_json['message'])
+        if repo_req:
+            with open(LOG_CSV, 'at', encoding = 'utf-8', newline ="") as loglist:
+                log_handle = csv.writer(loglist)
+                log_handle.writerow(["Error accessing url",url,repo_req.status_code,repo_json['message']])
+            return 0   
+        else:
+            print("Error code = UNKNOWN ",". Error message = UNKNOWN")
+            with open(LOG_CSV, 'at', encoding = 'utf-8', newline ="") as loglist:
+                log_handle = csv.writer(loglist)
+                log_handle.writerow(["Error accessing url","UNKNOWN","UNKNOWN"])
+            return 
+
+def ghpaginate(req):   
+    """This function checks the response packet header to see if there is a link for the "next" page. Returns the link if next page exists else None """
+    link = req.headers.get('link',None)
+#    print("LINK+++",link)
+    rel = ""
+    if link:
+        if 'rel="next"' in link: 
+            """ if there exists a next page, do this """
+            url_p = link.split('>; rel="next"')[0]
+            url = url_p.split('<')[-1]
+            return url
+        else:
+            return None
+    else:
+        return None 
+    
+def ghparse_row(repo_json, *items, prespace = 0):
+    """ Create row"""
+    repo_row = [None]
+    if prespace:
+        for i in range(int(prespace)):
+            repo_row.append("")
+    for item in items:  
+        if "*" in item: 
+            hitem = item.split("*")
+        else: hitem = [item]
+        
+        if repo_json[hitem[0]] is not None:
+            x = repo_json[hitem[0]]
+            for i in hitem[1:]:            
+                if x is not None:            
+                    x = x[i] 
+                else: 
+                    repo_row.append("Not Found") 
+                    break
+            if x is str:
+                repo_row.append(x.replace('\n',' ').replace('\r',' '))
+            else:
+                repo_row.append(x)
+        else:
+            repo_row.append("Not Found")
+    return repo_row
+
+if __name__ == '__main__':
+  main()
+  
---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 18:29:20 2019
+
+@author: kmpoo
+"""
+
+from setuptools import setup
+
+setup(name='poolib',
+      version='0.1',
+      description='Python module for different authentication functions',
+      url='https://github.com/km-Poonacha/PooLib',
+      author='poo',
+      author_email='km.poonacha@gmail.com',
+      license='GNU General Public License v3.0',
+      packages=['poolib'],
+      zip_safe=False)
